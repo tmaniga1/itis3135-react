e04912abfd44bcd5d2e88e6dc55ca69fa8428daf
tmaniga1 <tmaniga1@charlotte.edu>
2025-11-21 19:58:36 -0500

    Progress (11/21/2025)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Introduction from './Introduction.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+        <Route path="/introduction" element={<Introduction />}></Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
