2587f4472ba9bbd497358ba06805117421b4889e
tmaniga1 <tmaniga1@charlotte.edu>
2025-12-03 20:04:30 -0500

    Removed extra

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router";
 import Home from "./pages/Home";
 import Introduction from "./pages/Introduction";
 import Contract from "./pages/Contract";
